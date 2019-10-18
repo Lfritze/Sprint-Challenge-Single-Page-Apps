09529f9f9d334185050f94e3756fdd8f9753ab99
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-18 11:25:08 -0400

    added notes from API Docs and made API Request - setting up with styled-components

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,53 @@
 import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import styled from 'styled-components'
+
+// IMPORTANT!!! ****RICK & MORTY DOCS HERE: https://rickandmortyapi.com/documentation/******
+// Character info here: https://rickandmortyapi.com/documentation/#character ******
+//*****Notes from API Docs***** */
+//id	int	The id of the character.
+// name	string	The name of the character.
+// status	string	The status of the character ('Alive', 'Dead' or 'unknown').
+// species	string	The species of the character.
+
+
+const Content = styled.div`
+  max-width: 100%;
+`;
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characterData, setCharacterData] = useState ([])
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(response => {
+      const character = response.data.results
+      setCharacterData(character)
+      
+    })
+    .catch(error => {
+      console.log('Error returning data', error)
+    })
   }, []);
 
+
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    <section className="character-list"> {
+      characterData.map(item => {
+        return (
+          <div className = 'img-card'>
+            <Content key = {item.id}>
+
+              
+            </Content>
+          </div>
+        )
+      })
+    }
+      
+      
     </section>
   );
 }
